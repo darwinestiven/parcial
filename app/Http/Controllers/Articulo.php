a09ed751774be0ceb2eecd278bd37a24d044110f
orlandomonsal1 <orlandomonsal1@gmail.com>
2023-12-01 11:18:31 -0500

    Commit listado de marcas

---
+++ app/Http/Controllers/Articulo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class Articulo extends Controller
+{
+    public function index(){
+        $articulos = DB::table('articulo')
+                    ->join('marca', 'marca', '=', 'idMarca')
+                    ->get();
+        return view('articulos.listado', ['article' => $articulos]);
+    }
+    
+}
---
+++ resources/views/marcas/listado.blade.php
@@ -0,0 +1,46 @@
+@extends('adminlte::page')
+
+@section('title', 'Marcas')
+
+@section('content_header')
+    <h1>Listado de Marcas</h1>
+@stop
+
+@section('content')
+
+    <div style="text-align: right;">
+        <a href="/facultades/registrar" class="btn btn-success">Registrar</a>   
+    </div>
+    <br>
+
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">ID Marca</th>
+            <th scope="col">Descripción</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php
+                $i = 1;
+            @endphp
+            
+            @foreach($brand as $f)
+            <tr>
+            <th scope="row">{{$i}}</th>
+            <td>{{$f->idMarca}}</td>
+            <td>{{$f->descripcion}}</td>
+            
+            @php
+                $i = $i + 1;
+            @endphp
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
